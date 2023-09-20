de0df92301ad13182661f6caefeaec29eecf4f3b
Nicholas <gianfrancopr08@gmail.com>
2023-09-20 16:42:37 -0500

    Se crearon decoradores.

--- dist/js/views/view.js
+++ dist/js/views/view.js
@@ -1,21 +1,7 @@
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-import { medirTiempoEjecucion } from "../decorators/medir-tiempo-ejecucion.js";
-import { inspector } from "../decorators/inspector.js";
 export class View {
     constructor(selector, escapar) {
         this.escapar = false;
-        const elemento = document.querySelector(selector);
-        if (elemento) {
-            this.elemento = elemento;
-        }
-        else {
-            throw Error(`No existe ${selector} en el DOM. Por favor validar.`);
-        }
+        this.elemento = document.querySelector(selector);
         if (escapar)
             this.escapar = escapar;
     }
@@ -27,7 +13,3 @@ export class View {
         this.elemento.innerHTML = template;
     }
 }
-__decorate([
-    inspector(),
-    medirTiempoEjecucion()
-], View.prototype, "update", null);
