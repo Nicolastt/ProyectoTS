cf9b44b9bdc350dde4ea2d64716cc54481c50d3d
Nicholas <gianfrancopr08@gmail.com>
2023-09-19 23:02:06 -0500

    Se especificó el número de día en el enum.

--- app/enums/dias-semana.ts
+++ app/enums/dias-semana.ts
@@ -1,10 +1,13 @@
 // Cuando lo exportamos lo tenemos disponible para toda la aplicación.
-export enum DiasSemana{
-    DOMINGO,
-    LUNES,
-    MARTES,
-    MIERCOLES,
-    JUEVES,
-    VIERNES,
-    SABADO
+export enum DiasSemana {
+    // Hay que ser explícitos con los enums para evitar cualquier tipo de error a futuro.
+    // Así ya no depende del orden.
+    // ! Los enums son solo lectura.
+    DOMINGO = 0,
+    LUNES = 1,
+    MARTES = 2,
+    MIERCOLES = 3,
+    JUEVES = 4,
+    VIERNES = 5,
+    SABADO = 6
 }
